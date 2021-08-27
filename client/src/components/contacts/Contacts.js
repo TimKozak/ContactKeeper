c18c9df9e03f27a09c06b2cbacf1d0303a4682b0
Tim Kozak <tima.kozak@gmail.com>
2021-08-27 16:07:24 +0300

    finished project. preparing to heroku

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,55 @@
+import React, { Fragment, useContext, useEffect } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import ContactItem from "./ContactItem";
+import Spinner from "../layout/Spinner";
+
+import ContactContext from "../../context/contact/contactContext";
+
+const Contacts = () => {
+  const contactContext = useContext(ContactContext);
+
+  const { contacts, filtered, getContacts, loading } = contactContext;
+
+  useEffect(() => {
+    getContacts();
+    // eslint-disable-next-line
+  }, []);
+
+  if (contacts !== null && contacts.length === 0 && !loading) {
+    return <h4 className="card bg-light text-center">Please add a contact</h4>;
+  }
+
+  return (
+    <Fragment>
+      {contacts !== null && !loading ? (
+        <TransitionGroup>
+          {filtered !== null
+            ? filtered.map((contact) => (
+                // Key needs to go to the direct element if we are using map,
+                // so after transition we moved it to th CSSTransition component
+                <CSSTransition
+                  key={contact._id}
+                  timeout={500}
+                  classNames="item"
+                >
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              ))
+            : contacts.map((contact) => (
+                <CSSTransition
+                  key={contact._id}
+                  timeout={500}
+                  classNames="item"
+                >
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              ))}
+        </TransitionGroup>
+      ) : (
+        <Spinner />
+      )}
+    </Fragment>
+  );
+};
+
+export default Contacts;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Default import of express module without Webpack
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 // Initialize express application
 const app = express();
@@ -22,6 +23,17 @@ app.get("/", (req, res) => {
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  // Putting it below Defined Routes makes it get anything except those routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
